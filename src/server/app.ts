1f02d904f21d6a00750e8800fbd7f445425530d1
Slava Bezgachev <bezgachev@gmail.com>
2018-08-22 22:06:10 +0600

    Added body parser

--- src/server/app.ts
+++ src/server/app.ts
@@ -1,6 +1,9 @@
 import * as express from "express";
+import * as bodyParser from "body-parser";
 
 const app: express.Application = express();
+
+app.use(bodyParser.json());
 
 app.get("/", (req: express.Request, res: express.Response) => {
     return res.send({ message: "Hello there"} );
@@ -8,7 +11,7 @@ app.get("/", (req: express.Request, res: express.Response) => {
 
 app.post("/", (req: express.Request, res: express.Response) => {
     const parameters = req.body;
-    if (parameters.greeting === "Hello there"){
+    if (parameters && parameters.greeting === "Hello there"){
         return res.send({ message: "General Kenobi" });
     } else {
         return res.send({ message: "Hi!" })
