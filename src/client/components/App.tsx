db9531e337cf383cf8cb1f2cdc278a4061c0e9ea
Slava Bezgachev <bezgachev@gmail.com>
2018-08-23 19:03:04 +0600

    Simplified the React app

--- src/client/components/App.tsx
+++ src/client/components/App.tsx
@@ -1,41 +1,11 @@
 import * as React from "react";
-import * as axios from "axios";
 
-interface IAppState {
-  isLoading?: boolean;
-  requestString?: string;
-  response?: string;
-}
-
-class App extends React.Component<any, IAppState> {
-  constructor(props: any) {
-    super(props);
-    this.state = {
-      isLoading: false,
-      requestString: ""
-    };
-  }
-
-  handleInputChange(e: React.ChangeEvent<HTMLInputElement>) {
-    this.setState({
-      requestString: e.target && e.target.value ? e.target.value : ""
-    });
-  }
+class App extends React.Component {
 
   render() {
     return (
       <div>
         <h1>Welcome to the App</h1>
-        <form>
-          <label htmlFor="main_req">Submit to send a request</label>
-          <input
-            id="main_req"
-            type="text"
-            value={this.state.requestString}
-            onChange={this.handleInputChange.bind(this)}
-          />
-          <button type="submit">Send</button>
-        </form>
       </div>
     );
   }
