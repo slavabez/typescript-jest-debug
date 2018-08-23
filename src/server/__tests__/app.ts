26f64df23b360cd12fd17a5ac1e22bd2e4986402
Slava Bezgachev <bezgachev@gmail.com>
2018-08-23 17:50:17 +0600

    Backend tests finished

--- src/server/__tests__/app.ts
+++ src/server/__tests__/app.ts
@@ -6,6 +6,16 @@ describe("Back-end test suite", () => {
   test("A simple ping", async () => {
     const response = await supertest(app).get("/");
     expect(response.status).toBe(200);
+    expect(response.body).toEqual({ message: "Hello there" });
   });
-  test("Testing the POST with parameters", async () => {});
+  test("Testing the POST without parameters", async () => {
+    const response = await supertest(app).post("/");
+    expect(response.status).toBe(200);
+    expect(response.body).toEqual({ message: "Hi!" });
+  });
+  test("Testing the POST with parameters", async () => {
+    const response = await supertest(app).post("/").send({ greeting: "Hello there" });
+    expect(response.status).toBe(200);
+    expect(response.body).toEqual({ message: "General Kenobi" });
+  });
 });
